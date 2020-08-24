cdd062fda6d546ed5a7edeb2a86fa9256d8fe121
ablossomhead <aimeecapps@hotmail.com>
2020-08-24 16:31:24 -0400

    Updates

---
+++ precache-manifest.49eb4d76ff3a3092dd607a98f83bc9e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b8c8cc2d82354af23fdae9ba21e300",
+    "url": "/catsReact/index.html"
+  },
+  {
+    "revision": "3173c4130fd29becd15b",
+    "url": "/catsReact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6b1e925a2cfaa1db25d4",
+    "url": "/catsReact/static/css/main.3f90331c.chunk.css"
+  },
+  {
+    "revision": "3173c4130fd29becd15b",
+    "url": "/catsReact/static/js/2.2be654f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catsReact/static/js/2.2be654f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1e925a2cfaa1db25d4",
+    "url": "/catsReact/static/js/main.f6a2dd7b.chunk.js"
+  },
+  {
+    "revision": "e98913c9c135b15ed2bf",
+    "url": "/catsReact/static/js/runtime-main.7f5d0578.js"
+  },
+  {
+    "revision": "ac7908be2b39c3961e09b0b4d94e3170",
+    "url": "/catsReact/static/media/adopted.ac7908be.jpg"
+  },
+  {
+    "revision": "7ca9c2380c02145c8bac5629cfc06ac9",
+    "url": "/catsReact/static/media/adoptionday1.7ca9c238.jpg"
+  },
+  {
+    "revision": "243da250994857795f3ea38610d06c4e",
+    "url": "/catsReact/static/media/adoptionday2.243da250.jpg"
+  },
+  {
+    "revision": "30000122e6ca5d85b4f6811c6745fc16",
+    "url": "/catsReact/static/media/athena.30000122.jpg"
+  },
+  {
+    "revision": "73e354c4827413120876f81274106e0e",
+    "url": "/catsReact/static/media/athenaplays.73e354c4.gif"
+  },
+  {
+    "revision": "0d0bc641faf3196261f3e1055fcd4219",
+    "url": "/catsReact/static/media/family.0d0bc641.jpg"
+  },
+  {
+    "revision": "a7ed63d66d565cd3fc9e6c443dfe4e0d",
+    "url": "/catsReact/static/media/storm.a7ed63d6.jpg"
+  },
+  {
+    "revision": "2062db974474a389280a4ce65ed12176",
+    "url": "/catsReact/static/media/stormplays.2062db97.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/catsReact/precache-manifest.49eb4d76ff3a3092dd607a98f83bc9e1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/catsReact/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
